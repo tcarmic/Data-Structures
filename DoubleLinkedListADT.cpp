5eae6c6a47ac23a2f6f5d0b59d438d66607d1a7b
Thomas Carmichael <ThomasCarmichael5@gmail.com>
2018-04-22 17:37:46 -0500

    Update DoubleLinkedListADT.cpp
    Added ' void DeleteByValue(const T& data) ' to delete all instances of ' data ' from linked list.

--- DoubleLinkedListADT.cpp
+++ DoubleLinkedListADT.cpp
@@ -1,100 +1,159 @@
-// @Thomas - Double Linked List w/ ADT template implementation
+/*	
+	Thomas Carmichael 
+	ADT/Template Double Linked Link
+	https://github.com/tcarmic/Data-Structures/edit/master/DoubleLinkedListADT.cpp
+	
+	next:
+    1) function to sort template list in (nlog(n)) time, perhaps Mergesort
+        1a) function to insert an element into sorted list
+    	1b) function to delete all elements before/after an index/value in sorted list
+	2) circular linked list implementation
+*/
 
 template <class T>
-struct node {
-	T info;
-	node *next, *prev;
+struct Node {
+    T info;
+    Node *next, *prev;
 };
 
 template <class T>
-class linkedList {
+class LinkedList {
 private:
-	node<T> *head, *tail;
-
+    Node<T> *head, *tail;
 public:
-	linkedList() {
-		head = NULL;
-		tail = NULL;
-	}
+	
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+	
+    void PrintList() {
+        if (head == nullptr) {
+            std::cerr << "ERROR: Cannot print, list empty.\n";
+            return;
+        }
+        Node<T> *temp = new Node<T>;
+        temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->info << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
 
-	void printList() {
-		node<T> *temp = new node<T>;
-		temp = head;
-		while (temp != NULL) {
-			std::cout << temp->info << " ";
-			temp = temp->next;
-		}
-	}
-
-	void insertStart(T data) {
-		node<T> *temp = new node<T>;
-		temp->info = data;
-		temp->next = NULL;
-		temp->prev = NULL;
-		if (head == NULL) {
-			head = temp;
-			tail = temp;
-			temp = NULL;
-			return;
-		}
-		temp->next = head;
-		head->prev = temp;
-		head = temp;
-	}
-
-	void insertEnd(T data) {
-		node<T> *temp = new node<T>;
-		temp->info = data;
-		temp->next = NULL;
-		temp->prev = NULL;
-		if (head == NULL) {
-			head = temp;
-			tail = temp;
-			temp = NULL;
-			return;
-		}
-		tail->next = temp;
-		temp->prev = tail;
-		tail = temp;
-	}
-
-	void deleteFirst() {
-		if (head == NULL) {
-			std::cerr << "ERROR: Cannot delete, list empty.\n";
-			return;
-		}
-		node<T> *temp = head;
-		head = head->next;
-		head->prev = NULL;
-		delete temp;
-	}
-
-	void deleteLast() {
-		if (head == NULL) {
-			std::cerr << "ERROR: Cannot delete, list empty.\n";
-			return;
-		}
-		node<T> *temp = tail;
-		tail = tail->prev;
-		tail->next = NULL;
-		delete temp;
-	}
-
-	void deleteFullList() {
-		if (head == NULL) {
-			std::cerr << "ERROR: Cannot delete, list empty.\n";
-			return;
-		}
-		node<T> *temp = head;
-		node<T> *tempNext;
-		while (temp != NULL) {
-			tempNext = temp->next;
-			delete temp;
-			temp = tempNext;
-		}
-		head = NULL;
-		tail = NULL;
-	}
-
-	// next: add function to insert/delete a specific element or location in list
+    void InsertStart(const T &data) {
+        Node<T> *temp = new Node<T>;
+        temp->info = data;
+        temp->next = nullptr;
+        temp->prev = nullptr;
+        if (head == nullptr) {
+            head = temp;
+            tail = temp;
+            temp = nullptr;
+            return;
+        }
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+	
+    void InsertEnd(const T &data) {
+        Node<T> *temp = new Node<T>;
+        temp->info = data;
+        temp->next = nullptr;
+        temp->prev = nullptr;
+        if (head == nullptr) {
+            head = temp;
+            tail = temp;
+            temp = nullptr;
+            return;
+        }
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+	
+    void DeleteFirst() {
+        if (head == nullptr) {
+            std::cerr << "ERROR: Cannot delete, list empty.\n";
+            return;
+        }
+        Node<T> *temp = head;
+        head = head->next;
+        head->prev = nullptr;
+        temp = nullptr;
+    }
+	
+    void DeleteLast() {
+        if (head == nullptr) {
+            std::cerr << "ERROR: Cannot delete, list empty.\n";
+            return;
+        }
+        Node<T> *temp = tail;
+        tail = tail->prev;
+        tail->next = nullptr;
+        temp = nullptr;
+    }
+	
+    void DeleteFullList() {
+        if (head == nullptr) {
+            std::cerr << "ERROR: Cannot delete, list empty.\n";
+            return;
+        }
+        Node<T> *temp = head;
+        Node<T> *tempNext;
+        while (temp != nullptr) {
+            tempNext = temp->next;
+            delete temp;
+            temp = tempNext;
+        }
+        tempNext = nullptr;
+        head = nullptr;
+        tail = nullptr;
+    }
+	
+    void DeleteByValue(const T& data) {
+        if (head == nullptr) {
+            std::cerr << "ERROR: Cannot delete, list empty.\n";
+            return;
+        }
+        Node<T> *temp = head;
+        Node<T> *tempNext = nullptr;
+        if (temp->next == nullptr) {
+			
+            if (temp->info == data) {
+                head = nullptr;
+                temp = nullptr;
+                return;
+            }
+            return;
+        }
+        while (temp->info == data && temp != nullptr) {
+            head = head->next;
+            head->prev = nullptr;
+            temp = head;
+        }
+        tempNext = temp->next;
+        while (tempNext != nullptr) {
+			
+            if (tempNext->info == data) {
+				
+                if (tempNext->next == nullptr) {
+                    tail = tail->prev;
+                    tail->next = nullptr;
+                    temp = nullptr;
+                    tempNext = nullptr;
+                    return;
+                }
+                tempNext = tempNext->next;
+                temp->next = tempNext->prev = nullptr;
+                temp->next = tempNext;
+                tempNext->prev = temp;
+            }
+            else {
+                temp = temp->next;
+                tempNext = tempNext->next;
+            }
+        }
+    }
 };
